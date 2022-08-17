7aab1145545d788b039b15994954a0e568b46e54
Berat Sherif <beroosheriff@gmail.com>
2022-08-17 11:42:21 +0300

    Add: Methods Lab

---
+++ Programming Fundamentals with C#/07.Methods/1.Lab/Methods - Lab/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = Math.Abs(int.Parse(Console.ReadLine()));
+
+            int evenSum = GetSumOfEvenDigits(input);
+            int oddSum = GetSumOfOddDigits(input);
+            int result = GetMultipleOfEvenAndOdds(evenSum, oddSum);
+
+            Console.WriteLine(result);
+
+        }
+
+        static int GetSumOfEvenDigits(int input)
+        {
+            int evenSum = 0;
+            int currNumber = input;
+            while (currNumber > 0)
+            {
+                int digit = currNumber % 10;
+                if (digit % 2 == 0)
+                {
+                    evenSum += digit;
+                }
+                currNumber /= 10;
+            }
+
+            return evenSum;
+        }
+
+        static int GetSumOfOddDigits (int input)
+        {
+            int oddSum = 0;
+            int currNumber = input;
+            while (currNumber > 0)
+            {
+                int digit = currNumber % 10;
+                if (digit % 2 != 0)
+                {
+                    oddSum += digit;
+                }
+                currNumber /= 10;
+            }
+
+            return oddSum;
+        }
+
+        static int GetMultipleOfEvenAndOdds(int evenSum, int oddSum)
+        {
+            return evenSum * oddSum;
+        }
+    }
+}
