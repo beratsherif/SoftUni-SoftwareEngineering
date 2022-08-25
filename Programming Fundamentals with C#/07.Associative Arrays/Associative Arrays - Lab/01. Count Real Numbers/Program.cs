2eee00e64f1cebda03ec697e6443021a1c66e049
Berat Sherif <beroosheriff@gmail.com>
2022-08-25 15:33:19 +0300

    Add:Associative Arrays Lab

---
+++ Programming Fundamentals with C#/07.Associative Arrays/Associative Arrays - Lab/01. Count Real Numbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SortedDictionary<double, int> ocurencesByNumber = new SortedDictionary<double, int>();
+
+            double[] numbers = Console.ReadLine().Split().Select(double.Parse).ToArray();
+
+            foreach (var number in numbers)
+            {
+                if (!ocurencesByNumber.ContainsKey(number))
+                {
+                    ocurencesByNumber.Add(number, 0);
+                }
+
+                ocurencesByNumber[number]++;
+            }
+
+            foreach (var ocurenceByNumber in ocurencesByNumber)
+            {
+                Console.WriteLine($"{ocurenceByNumber.Key} -> {ocurenceByNumber.Value}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/07.Associative Arrays/Associative Arrays - Lab/02. Odd Occurrences/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Odd_Occurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] words = Console.ReadLine().Split();
+            //Dictionary<string, int> counts = new Dictionary<string, int>();
+
+            //foreach (var word in words)
+            //{
+            //    string wordInLowerCase = word.ToLower();
+            //    if (counts.ContainsKey(wordInLowerCase))
+            //    {
+            //        counts[wordInLowerCase]++;
+            //    }
+            //    else
+            //    {
+            //        counts.Add(wordInLowerCase, 1);
+            //    }
+            //}
+
+            //foreach (var count in counts)
+            //{
+            //    if (count.Value % 2 != 0)
+            //    {
+            //        Console.WriteLine(count.Key + " ");
+            //    }
+            //}
+
+
+            Dictionary<string, int> wordsCounts = new Dictionary<string, int>();
+
+            string[] words = Console.ReadLine().Split().Select(word => word.ToLower()).ToArray();
+
+            foreach (var word in words)
+            {
+                if (!wordsCounts.ContainsKey(word))
+                {
+                    wordsCounts.Add(word, 0);
+                }
+
+                wordsCounts[word]++;
+            }
+            
+            string[] oddCountWords = wordsCounts.Where(w => w.Value % 2 != 0).Select(w => w.Key).ToArray();
+            Console.WriteLine(string.Join(" ", oddCountWords));
+        }
+    }
+}
