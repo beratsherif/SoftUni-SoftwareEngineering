0ea73a64a1849003c27cb5666b6d7a17e2e88c6c
Berat Sherif <beroosheriff@gmail.com>
2022-09-02 14:12:37 +0300

    Add:Regular Expressions - Lab

---
+++ Programming Fundamentals with C#/09.Regular Expressions/Regular Expressions -Lab/2. Match Phone Number/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2._Match_Phone_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\+359(?<sep>[ \-])2(\k<sep>)([0-9]{3})(\k<sep>)([0-9]{4})\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/09.Regular Expressions/Regular Expressions -Lab/3. Match Dates/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3._Match_Dates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"\b(?<day>\d{2})(?<separator>[-.\/])(?<month>[A-Z][a-z]{2})\k<separator>(?<year>\d{4})\b";
+
+            MatchCollection matches = Regex.Matches(input, pattern);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine($"Day: {match.Groups["day"]}, Month: {match.Groups["month"]}, Year: {match.Groups["year"]}");
+            }
+        }
+    }
+}
