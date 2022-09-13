ef7489558078461a4c65011c233ab99fcaa3a70c
Berat Sherif <beroosheriff@gmail.com>
2022-09-13 11:06:40 +0300

    Add:Object and Classes More Exercises

---
+++ Programming Fundamentals with C#/06.Objects and Classes/Objects and Classes - More Exercise/02.OldestFamilyMember/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OldestFamilyMember
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int lines = int.Parse(Console.ReadLine());
+
+            var family = new Family();
+
+            for (int i = 0; i < lines; i++)
+            {
+                var line = Console.ReadLine().Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var person = new Person(line[0], int.Parse(line[1]));
+
+                family.AddMember(person);
+            }
+
+            var oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
+        }
+    }
+
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set { this.age = value; }
+        }
+
+        public Person()
+        {
+            this.Name = "No name";
+            this.Age = 1;
+        }
+
+        public Person(int age)
+        {
+            this.Name = "No name";
+            this.Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+    }
+
+    public class Family
+    {
+        private List<Person> membersList;
+
+        public List<Person> MembersList
+        {
+            get { return membersList; }
+            set { membersList = value; }
+        }
+
+        public Family()
+        {
+            this.MembersList = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            this.MembersList.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return this.membersList.OrderByDescending(p => p.Age).FirstOrDefault();
+        }
+    }
+}
---
+++ Programming Fundamentals with C#/06.Objects and Classes/Objects and Classes - More Exercise/05.ShoppingSpree/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime;
+
+namespace _05.ShoppingSpree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+            List<Product> products = new List<Product>();
+
+            string[] peopleInput = Console.ReadLine().Split(';',StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var person in peopleInput)
+            {
+                string name = person.Split('=')[0];
+                decimal money = decimal.Parse(person.Split('=')[1]);
+                people.Add(new Person(name, money));
+            }// PEOPLE
+
+            string[] productsInput = Console.ReadLine().Split(';',StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var product in productsInput)
+            {
+                string productName = product.Split('=')[0];
+                decimal productPrice = decimal.Parse(product.Split('=')[1]);
+                products.Add(new Product(productName, productPrice));
+            }// PRODUCTS
+
+            string input = Console.ReadLine();
+            while (input != "END")
+            {
+                string[] commands = input.Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                string personName = commands[0];
+                string productName = commands[1];
+
+                people.Find(p => p.PersonName == personName).BuyProduct(products.Find(p => p.ProductName == productName));
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var person in people)
+            {
+                if (person.Bag.Count > 0)
+                {
+                    Console.WriteLine($"{person.PersonName} - {string.Join(", ",person.Bag)}");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.PersonName} - Nothing bought");
+                }
+            }
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, decimal money)
+        {
+            this.PersonName = name;
+            this.Money = money;
+        }
+        public string PersonName { get; set; }
+        public decimal Money { get; set; }
+        public List<string> Bag { get; set; } = new List<string>();
+
+        public void BuyProduct(Product productToBuy)
+        {
+            if (this.Money >= productToBuy.ProductPrice)
+            {
+                this.Bag.Add(productToBuy.ProductName);
+                this.Money -= productToBuy.ProductPrice;
+                Console.WriteLine($"{this.PersonName} bought {productToBuy.ProductName}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.PersonName} can't afford {productToBuy.ProductName}");
+            }
+        }
+    }
+
+    class Product
+    {
+        public Product(string productName,decimal productPrice)
+        {
+            this.ProductName = productName;
+            this.ProductPrice = productPrice;
+        }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
