e1b5337a93e01f9f09c04353d90c399203554740
Berat Sherif <beroosheriff@gmail.com>
2022-10-04 19:03:10 +0300

    Add:6th and 7th exercises from sets and dictionaries advanced.

---
+++ C# Advanced/3.Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Lab/06. Record Unique Names/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06._Record_Unique_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> uniqueNamesList = new HashSet<string>();
+
+            int inputForForCycle = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < inputForForCycle; i++)
+            {
+                string inputForSet = Console.ReadLine();
+                uniqueNamesList.Add(inputForSet);
+            }
+
+            //Printing the set
+            foreach (var name in uniqueNamesList)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
---
+++ C# Advanced/3.Sets and Dictionaries Advanced/Sets and Dictionaries Advanced - Lab/07. Parking Lot/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07._Parking_Lot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> carsInParking = new HashSet<string>();
+
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(", ");
+                if (command[0] == "END")
+                {
+                    break;
+                }
+
+                string direction = command[0];
+                string carNumber = command[1];
+
+                switch (direction)
+                {
+                    case "IN":
+                        carsInParking.Add(carNumber);
+                        break;
+                    case "OUT":
+                        carsInParking.Remove(carNumber);
+                        break;
+                }
+            }
+
+            if (carsInParking.Count > 0)
+            {
+                foreach (var car in carsInParking)
+                {
+                    Console.WriteLine(car);
+                }
+            }
+            else
+                Console.WriteLine("Parking Lot is Empty");
+        }
+    }
+}
