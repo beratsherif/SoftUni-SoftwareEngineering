40fdf5886c0be20ae196371fbd2126fc2b4a48e6
Berat Sherif <beroosheriff@gmail.com>
2022-10-12 16:23:58 +0300

    Add:Streams, Files and Directories Lab

---
+++ C# Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/02. Line Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace LineNumbers
+{
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+        public static void RewriteFileWithLineNumbers(string inputPath, string outputPath)
+        {
+            using (StreamReader reader = new StreamReader(inputPath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputPath))
+                {
+                    int counter = 1;
+                    while (!reader.EndOfStream)
+                    {
+                        string line = reader.ReadLine();
+                        writer.WriteLine($"{counter++}. {line}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Lab/03. Word Count/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace WordCount
+{
+    public class WordCount
+    {
+        static void Main()
+        {
+            string wordPath = @"..\..\..\Files\words.txt";
+            string textPath = @"..\..\..\Files\text.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+            CalculateWordCounts(wordPath, textPath, outputPath);
+        }
+
+        public static void CalculateWordCounts(string wordsFilePath, string textFilePath, string outputFilePath)
+        {
+            SortedDictionary<string, int> counter = new SortedDictionary<string, int>();
+            string text = File.ReadAllText(wordsFilePath);
+            string[] words = text.Split();
+            using (StreamReader reader = new StreamReader(textFilePath))
+            {
+                using (StreamWriter writer = new StreamWriter(outputFilePath))
+                {
+                    string curLine = reader.ReadLine();
+                    while (curLine != null)
+                    {
+                        foreach (var word in words)
+                        {
+                            if (curLine.ToLower().Contains(word))
+                            {
+                                if (!counter.ContainsKey(word))
+                                {
+                                    counter.Add(word,0);
+                                }
+
+                                counter[word]++;
+                            }
+                        }
+
+                        curLine = reader.ReadLine();
+                    }
+
+                    foreach (var word in counter.OrderByDescending(x => x.Value))
+                    {
+                        writer.WriteLine($"{word.Key} - {word.Value}");
+                    }
+                }
+            }
+        }
+    }
+
+}
