97f11ce0d36da6bd204ed72cbc3a7488b6a5f741
Berat Sherif <beroosheriff@gmail.com>
2022-10-16 23:13:48 +0300

    Add:Exercises

---
+++ C# Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Exercises/EvenLines/EvenLines.cs
@@ -0,0 +1,66 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace EvenLines
+{
+    using System;
+    public class EvenLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+
+            Console.WriteLine(ProcessLines(inputFilePath));
+        }
+
+        public static string ProcessLines(string inputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(inputFilePath))
+            {
+                string line = string.Empty;
+                int counter = 0;
+
+                while (line != null)
+                {
+                    line = reader.ReadLine();
+
+                    if (counter % 2 == 0)
+                    {
+                        string replacedSymbols = ReplaceSymbols(line);
+                        string reverseWords = ReverseWords(replacedSymbols);
+                        sb.AppendLine(reverseWords);
+                    }
+
+                    counter++;
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static string ReplaceSymbols(string line)
+        {
+            string[] symbolsToReplace = { "-", ",", ".", "!", "?" };
+            foreach (var symbol in symbolsToReplace)
+            {
+                line = line.Replace(symbol, "@");
+            }
+
+            return line;
+        }
+
+        private static string ReverseWords(string replacedSymbols)
+        {
+            string[] reversedWords = replacedSymbols
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+
+            return string.Join(" ", reversedWords);
+        }
+
+    }
+}
---
+++ C# Advanced/4.Streams, Files and Directories/Streams, Files and Directories - Exercises/LineNumbers/LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace LineNumbers
+{
+    using System;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string[] lines = File.ReadAllLines(inputFilePath);
+
+            for (int i = 0; i < lines.Length; i++)
+            {
+                int letterCounter = lines[i].Count(ch => char.IsLetter(ch));
+                int symbolCOunter = lines[i].Count(sy => char.IsPunctuation(sy));
+
+                sb.AppendLine($"Line {i + 1}: {lines[i]} {letterCounter}{symbolCOunter}");
+            }
+
+            File.WriteAllText(outputFilePath, sb.ToString());
+        }
+    }
+}
