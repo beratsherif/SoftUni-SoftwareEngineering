aa2113d1d779a81f0e49d22932916f3b2f11daee
Berat Sherif <beroosheriff@gmail.com>
2022-10-17 20:42:19 +0300

    Add:Functional Programming Lab folder and exercises.

---
+++ C# Advanced/5.Functional Programming/Functional Programming - Lab/03. Count Uppercase Words/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03._Count_Uppercase_Words
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<string> isItUpperCase = ch => char.IsUpper(ch[0]);
+            //version 1
+            //List<string> input = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToList();
+            //List<string> result = input.FindAll(isItUpperCase).ToList();
+
+            //foreach (var re in result)
+            //{
+            //    Console.WriteLine(re);
+            //}
+
+            //version 2
+            Console.WriteLine(string.Join(Environment.NewLine,Array.FindAll(Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries),isItUpperCase)));
+        }
+    }
+}
