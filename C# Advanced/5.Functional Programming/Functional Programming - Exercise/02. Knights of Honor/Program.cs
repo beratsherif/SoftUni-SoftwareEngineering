38d3b8482ac65be2ec5a341940f6d6a0368977df
Berat Sherif <beroosheriff@gmail.com>
2022-10-20 11:56:13 +0300

    Add:Functional Programming Exercises Folder and tasks

---
+++ C# Advanced/5.Functional Programming/Functional Programming - Exercise/02. Knights of Honor/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _02._Knights_of_Honor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[], string> sirString = (strings, title) =>
+            {
+                foreach (var name in strings)
+                {
+                    Console.WriteLine($"{title} {name}");
+                }
+            };
+
+            string[] names = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            sirString(names, "Sir");
+        }
+    }
+}
---
+++ C# Advanced/5.Functional Programming/Functional Programming - Exercise/03. Custom Min Function/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace _03._Custom_Min_Function
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> smallestNum = nums =>
+            {
+
+                int min = int.MaxValue;
+                foreach (var num in nums)
+                {
+                    if (num < min)
+                    {
+                        min = num;
+                    }
+                }
+
+                return min;
+            };
+
+            int[] numbers = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(smallestNum(numbers));
+        }
+    }
+}
