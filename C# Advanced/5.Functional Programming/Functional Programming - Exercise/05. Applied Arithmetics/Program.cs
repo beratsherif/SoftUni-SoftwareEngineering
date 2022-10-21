ddd6677df6ec6f7617ae363d67ace984edc6b96f
Berat Sherif <beroosheriff@gmail.com>
2022-10-21 19:47:11 +0300

    Add:Exercises

---
+++ C# Advanced/5.Functional Programming/Functional Programming - Exercise/05. Applied Arithmetics/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net.Http;
+
+namespace _05._Applied_Arithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<List<int>> add = numbers =>
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numbers[i]++;
+                }
+            };
+
+            Action<List<int>> multiply = numbers =>
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numbers[i] *= 2;
+                }
+            };
+
+            Action<List<int>> subtract = numbers =>
+            {
+                for (int i = 0; i < numbers.Count; i++)
+                {
+                    numbers[i]--;
+                }
+            };
+
+            Action<List<int>> print = numbers => Console.WriteLine(string.Join(" ", numbers));
+
+
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            while (true)
+            {
+                string cmd = Console.ReadLine();
+                if (cmd == "end")
+                {
+                    break;
+                }
+
+                switch (cmd)
+                {
+                    case "add":
+                        add(numbers);
+                        break;
+                    case "multiply":
+                        multiply(numbers);
+                        break;
+                    case "subtract":
+                        subtract(numbers);
+                        break;
+                    case "print":
+                        print(numbers);
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/5.Functional Programming/Functional Programming - Exercise/06. Reverse And Exclude/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06._Reverse_And_Exclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> inputNums = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int n = int.Parse(Console.ReadLine()); // Input.
+
+
+            Func<List<int>, List<int>> reverse = numbers => // Reverse Func.
+            {
+                List<int> reversedResult = new List<int>();
+
+                for (int i = numbers.Count - 1; i >= 0; i--)
+                {
+                    reversedResult.Add(numbers[i]);
+                }
+
+                return reversedResult;
+            };
+
+            Func<List<int>, Predicate<int>, List<int>> removedResut = (numbers, match) => // Func for result.
+            {
+                List<int> removedList = new List<int>();
+
+                foreach (var num in numbers)
+                {
+                    if (!match(num))
+                    {
+                        removedList.Add(num);
+                    }
+                }
+                return removedList;
+            };
+
+            inputNums = removedResut(inputNums, num => num % n == 0);
+            inputNums = reverse(inputNums);
+            Console.WriteLine(string.Join(" ", inputNums));
+        }
+    }
+}
