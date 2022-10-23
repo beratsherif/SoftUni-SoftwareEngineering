6fc5fb5be7fb0661fc73a52ab0a8ab42ae255665
Berat Sherif <beroosheriff@gmail.com>
2022-10-23 23:11:30 +0300

    Add:Exercises

---
+++ C# Advanced/5.Functional Programming/Functional Programming - Exercise/07. Predicate For Names/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07._Predicate_For_Names
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[], Predicate<string>> printNames = (names, match) =>
+            {
+                foreach (var name in names)
+                {
+                    if (match(name))
+                    {
+                        Console.WriteLine(name);
+                    }
+                }
+            };
+
+            int maxNameLenght = int.Parse(Console.ReadLine());
+            string[] input = Console.ReadLine().Split();
+
+            printNames(input, n => n.Length <= maxNameLenght);
+        }
+    }
+}
---
+++ C# Advanced/5.Functional Programming/Functional Programming - Exercise/08. List Of Predicates/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace _08._List_Of_Predicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Action<int, HashSet<int>> printTheResult = (endOfRange, dividers) =>
+            //{
+            //    for (int i = 1; i <= endOfRange; i++)
+            //    {
+            //        int curNum = i;
+            //        bool check = true;
+            //        foreach (var divider in dividers)
+            //        {
+            //            if (i % divider != 0)
+            //            {
+            //                check = false;
+            //                break;
+            //            }
+            //        }
+
+            //        if (check)
+            //        {
+            //            Console.Write($"{i} ");
+            //        }
+            //    }
+            //};
+
+            //int endOfRange = int.Parse(Console.ReadLine());
+            //HashSet<int> dividers = Console.ReadLine().Split().Select(int.Parse).ToHashSet();
+
+            //printTheResult(endOfRange, dividers);
+
+
+
+            List<Predicate<int>> predicates = new List<Predicate<int>>();
+
+            int endRange = int.Parse(Console.ReadLine());
+
+            HashSet<int> dividers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToHashSet();
+
+            int[] numbers = Enumerable.Range(1, endRange).ToArray();
+
+            foreach (var divider in dividers)
+            {
+                predicates.Add(p => p % divider == 0);
+            }
+
+            foreach (var number in numbers)
+            {
+                bool isDivisible = true;
+
+                foreach (var match in predicates)
+                {
+                    if (!match(number))
+                    {
+                        isDivisible = false;
+                        break;
+                    }
+                }
+
+                if (isDivisible)
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
