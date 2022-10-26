ea0c9a9013cc32931a33c37edda7a17f961efdd7
Berat Sherif <beroosheriff@gmail.com>
2022-10-26 20:54:47 +0300

    Add:Defining Classes Folder with 2 Lab tasks

---
+++ C# Advanced/6.Defining Classes/Defining Classes - Lab/01. Car/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01._Car
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car();
+            car1.Make = "Mercedes-Benz";
+            car1.Model = "c220 CDI";
+            car1.Year = 2007;
+
+            Console.WriteLine($"Make:{car1.Make} Model:{car1.Model} Year:{car1.Year}");
+        }
+    }
+}
---
+++ C# Advanced/6.Defining Classes/Defining Classes - Lab/02. Car Extension/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _02._Car_Extension
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+            car.Make = "Mercedes-Benz";
+            car.Model = "c220 CDI";
+            car.Year = 2007;
+            car.FuelQuantity = 66;
+            car.FuelConsumption = 2;
+            car.Drive(30);
+            Console.WriteLine(car.WhoAmI());
+        }
+    }
+}
