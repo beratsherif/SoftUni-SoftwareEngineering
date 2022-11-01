450eeb8d473067080832055356f30fae2c6235ad
Berat Sherif <beroosheriff@gmail.com>
2022-11-01 16:14:41 +0200

    Add:Fifth task

---
+++ C# Advanced/6.Defining Classes/Defining Classes - Lab/05. Special Cars/Car.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05._Special_Cars
+{
+    public class Car
+    {
+        public Car(string make,string model,int year,double fuelQuantity,double fuelConsumption,Engine engine,Tire[] tires)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+            this.Engine = engine;
+            this.Tires = tires;
+        }
+        private string make;
+        private string model;
+        private int year;
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private Engine engine;
+        private Tire[] tires;
+
+        public string Make
+        {
+            get { return this.make; }
+            set { this.make = value; }
+        }
+
+        public string Model
+        {
+            get { return this.model; }
+            set { this.model = value; }
+        }
+
+        public int Year
+        {
+            get { return this.year; }
+            set { this.year = value; }
+        }
+
+        public double FuelQuantity
+        {
+            get { return this.fuelQuantity; }
+            set { this.fuelQuantity = value; }
+        }
+
+        public double FuelConsumption
+        {
+            get { return this.fuelConsumption; }
+            set { this.fuelConsumption = value; }
+        }
+
+        public Engine Engine
+        {
+            get { return this.engine; }
+            set { this.engine = value; }
+        }
+
+        public Tire[] Tires
+        {
+            get { return this.tires; }
+            set { this.tires = value; }
+        }
+        public void Drive(double distance)
+        {
+            if (FuelQuantity - ((FuelConsumption / 100) * distance) >= 0)
+            {
+                FuelQuantity -= (FuelConsumption / 100) * distance;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform this trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nHorsePowers:{this.Engine.HorsePower}\nFuelQuantity: {this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ C# Advanced/6.Defining Classes/Defining Classes - Lab/05. Special Cars/StartUp.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05._Special_Cars
+{
+    class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            List<Tire[]> tires = new List<Tire[]>();
+            List<Engine> engines = new List<Engine>();
+            List<Car> cars = new List<Car>();
+
+            string firstInput = Console.ReadLine();
+            while (firstInput != "No more tires")
+            {
+                string[] data = firstInput.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Tire[] currTires = new Tire[4]
+                {
+                    new Tire(int.Parse(data[0]),double.Parse(data[1])),
+                    new Tire(int.Parse(data[2]),double.Parse(data[3])),
+                    new Tire(int.Parse(data[4]),double.Parse(data[5])),
+                    new Tire(int.Parse(data[6]),double.Parse(data[7]))
+                };
+                tires.Add(currTires);
+
+                firstInput = Console.ReadLine();
+            }
+
+            string secondInput = Console.ReadLine();
+            while (secondInput != "Engines done")
+            {
+                string[] data = secondInput.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Engine currEngine = new Engine(int.Parse(data[0]), double.Parse(data[1]));
+                engines.Add(currEngine);
+
+                secondInput = Console.ReadLine();
+            }
+
+            string thirdInput = Console.ReadLine();
+            while (thirdInput != "Show special")
+            {
+                string[] data = thirdInput.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string make = data[0];
+                string model = data[1];
+                int year = int.Parse(data[2]);
+                double fuelQuantity = double.Parse(data[3]);
+                double fuelConsumption = double.Parse(data[4]);
+                int engineIndex = int.Parse(data[5]);
+                int tiresIndex = int.Parse(data[6]);
+
+                Car currCar = new Car(make, model, year, fuelQuantity, fuelConsumption, engines[engineIndex], tires[tiresIndex]);
+                cars.Add(currCar);
+
+                thirdInput = Console.ReadLine();
+            }
+
+            cars = cars.Where(c =>
+                c.Year >= 2017 
+                && c.Engine.HorsePower > 330 
+                && c.Tires.Sum(y => y.Pressure) >= 9 
+                &&
+                c.Tires.Sum(y => y.Pressure) <= 10)
+                .ToList();
+
+            foreach (var car in cars)
+            {
+                car.Drive(20);
+                Console.WriteLine(car.WhoAmI());
+            }
+        }
+    }
+}
