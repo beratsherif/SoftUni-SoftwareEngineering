28d896d82bfe8772e6b5c12e954820d2f864e9df
Berat Sherif <beroosheriff@gmail.com>
2022-11-02 23:19:59 +0200

    Add:Folders and generics - lab task

---
+++ C# Advanced/9.Generics/Generics - Lab/01. Box/Box.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01._Box
+{
+    //public class Box<T>
+    //{
+    //    private List<T> list;
+
+    //    public Box()
+    //    {
+    //        list = new List<T>();
+    //    }
+
+    //    public int Count
+    //    {
+    //        get { return this.list.Count; }
+    //    }
+
+    //    public void Add(T element)
+    //    {
+    //        list.Insert(Count,element);
+    //    }
+
+    //    public T Remove()
+    //    {
+    //        T elementToRemove = list[Count - 1];
+    //        list.RemoveAt(Count - 1);
+    //        return elementToRemove;
+    //    }
+    //}
+
+    public class Box<T>
+    {
+        private Stack<T> list;
+        public Box()
+        {
+            list = new Stack<T>();
+        }
+        public int Count
+        {
+            get { return list.Count; }
+        }
+
+        public void Add(T element)
+        {
+            list.Push(element);
+        }
+
+        public T Remove()
+        {
+            T currElement = list.Pop();
+            return currElement;
+        }
+    }
+
+}
---
+++ C# Advanced/9.Generics/Generics - Lab/01. Box/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _01._Box
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Version 1
+
+            //Box<int> box = new Box<int>();
+            //box.Add(1);
+            //box.Add(2);
+            //Console.WriteLine(box.Remove());
+
+
+            //Box<string> box2 = new Box<string>();
+            //box2.Add("Hello");
+            //box2.Add("World");
+            //Console.WriteLine(box2.Remove());
+
+            //Console.WriteLine(box2.Count);
+
+            //Version 2
+
+            Box<int> box = new Box<int>();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+
+        }
+    }
+}
