3926f54c756881fe3f83a0e23c892a7e1f510ff4
Berat Sherif <beroosheriff@gmail.com>
2022-11-03 22:42:46 +0200

    Add:Generics-Lab tasks.

---
+++ C# Advanced/9.Generics/Generics - Lab/02. Array Creator/ArrayCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+namespace _02._Array_Creator
+{
+    public class ArrayCreator
+    {
+        public static T[] Create<T>(int lenght, T item)
+        {
+            T[] array = new T[lenght];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = item;
+            }
+
+            return array;
+        }
+    }
+}
---
+++ C# Advanced/9.Generics/Generics - Lab/03. Generic Scale/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _03._Generic_Scale
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> scale = new EqualityScale<int>(5, 5);
+            Console.WriteLine(scale.AreEqual());
+
+            EqualityScale<string> scale2 = new EqualityScale<string>("Hello", "hello");
+            Console.WriteLine(scale2.AreEqual());
+        }
+    }
+}
