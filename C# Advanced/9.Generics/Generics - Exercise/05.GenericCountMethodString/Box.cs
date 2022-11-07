7c5f978847082ded3982531e60b74b0ae14a11a4
Berat Sherif <beroosheriff@gmail.com>
2022-11-07 23:08:05 +0200

    Add:new tasks

---
+++ C# Advanced/9.Generics/Generics - Exercise/05.GenericCountMethodString/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _05.GenericCountMethodString
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        public Box()
+        {
+            Items = new List<T>();
+        }
+        public List<T> Items { get; set; }
+
+        public int Count(T itemToCompare)
+        {
+            int count = 0;
+            foreach (var item in Items)
+            {
+                if (item.CompareTo(itemToCompare) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+    }
+}
---
+++ C# Advanced/9.Generics/Generics - Exercise/05.GenericCountMethodString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05.GenericCountMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new Box<string>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+                box.Items.Add(input);
+            }
+
+            string itemToCompare = Console.ReadLine();
+
+            Console.WriteLine(box.Count(itemToCompare));
+        }
+    }
+}
