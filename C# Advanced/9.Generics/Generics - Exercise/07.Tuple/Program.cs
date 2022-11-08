b7df21f727338db6bed9f58582d37beed63a0ae4
Berat Sherif <beroosheriff@gmail.com>
2022-11-08 19:42:13 +0200

    Add:new tasks

---
+++ C# Advanced/9.Generics/Generics - Exercise/07.Tuple/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _07.Tuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] personsInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] drinkInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] numbersInfo = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Tuple<string, string> nameAddress = new Tuple<string, string>();
+            nameAddress.Item1 = $"{personsInfo[0]} {personsInfo[1]}";
+            nameAddress.Item2 = personsInfo[2];
+
+            Tuple<string, int> nameBeer = new Tuple<string, int>();
+            nameBeer.Item1 = drinkInfo[0];
+            nameBeer.Item2 = int.Parse(drinkInfo[1]);
+
+            Tuple<int, double> numbers = new Tuple<int, double>();
+            numbers.Item1 = int.Parse(numbersInfo[0]);
+            numbers.Item2 = double.Parse(numbersInfo[1]);
+
+            Console.WriteLine(nameAddress.ToString());
+            Console.WriteLine(nameBeer.ToString());
+            Console.WriteLine(numbers.ToString());
+        }
+    }
+}
---
+++ C# Advanced/9.Generics/Generics - Exercise/08.Threeuple/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08.Threeuple
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input1 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] input2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] input3 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+            Threeuple<string, string, string> nameAddressTown = new Threeuple<string, string, string>();
+            nameAddressTown.Item1 = $"{input1[0]} {input1[1]}";
+            nameAddressTown.Item2 = input1[2];
+            if (input1.Length > 4) nameAddressTown.Item3 = string.Join(" ", input1[3], input1[4]);
+            else nameAddressTown.Item3 = input1[3];
+
+            Threeuple<string, int, bool> nameLittresOfBeerDrunkOrNot = new Threeuple<string, int, bool>
+            {
+                Item1 = input2[0],
+                Item2 = int.Parse(input2[1]),
+                Item3 = input2[2] == "drunk"
+            };
+
+            Threeuple<string, double, string> nameAccountBalanceBankName = new Threeuple<string, double, string>
+            {
+                Item1 = input3[0],
+                Item2 = double.Parse(input3[1]),
+                Item3 = input3[2]
+            };
+
+            Console.WriteLine(nameAddressTown.ToString());
+            Console.WriteLine(nameLittresOfBeerDrunkOrNot.ToString());
+            Console.WriteLine(nameAccountBalanceBankName.ToString());
+        }
+    }
+}
