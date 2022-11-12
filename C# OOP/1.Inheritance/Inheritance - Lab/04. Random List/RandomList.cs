5c6e875f49520b2c4e656d8c870f637514e144dd
Berat Sherif <beroosheriff@gmail.com>
2022-11-12 18:47:30 +0200

    Add:New tasks

---
+++ C# OOP/1.Inheritance/Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04._Random_List
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+
+        public RandomList()
+        {
+            random = new Random();
+        }
+
+        public string RandomString()
+        {
+            int index = random.Next(0, this.Count);
+
+            string removedElement = this[index];
+
+            this.RemoveAt(index);
+
+            return removedElement;
+        }
+    }
+}
---
+++ C# OOP/1.Inheritance/Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _04._Random_List
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+            randomList.Add("1");
+            randomList.Add("2");
+            randomList.Add("3");
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
