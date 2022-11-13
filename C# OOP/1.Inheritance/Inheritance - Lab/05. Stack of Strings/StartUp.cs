798a73cc88190c0547b839f717124e19e1dda32e
Berat Sherif <beroosheriff@gmail.com>
2022-11-13 23:34:16 +0200

    Add:new task

---
+++ C# OOP/1.Inheritance/Inheritance - Lab/05. Stack of Strings/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05._Stack_of_Strings
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+            stackOfStrings.AddRange(new List<string> { "1", "2", "3", "4", "5" });
+
+            while (!stackOfStrings.IsEmpty())
+            {
+                Console.WriteLine(stackOfStrings.Pop());
+            }
+        }
+    }
+}
