757264c3ae34505a1aa08085f4600a28ee919826
Berat Sherif <beroosheriff@gmail.com>
2022-11-19 23:24:39 +0200

    Add:Encapsulation - Lab

---
+++ C# OOP/2.Encapsulation/Encapsulation - Lab/02.Salary/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace _02.Salary
+{
+    public class Person
+    {
+        public Person(string firstName,string lastName,int age,decimal salary)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+            this.Salary = salary;
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int  Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            this.Salary += Salary * percentage / 100;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:f2} leva.";
+        }
+    }
+}
