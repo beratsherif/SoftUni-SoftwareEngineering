b01f118c81a01c30cb0e44abfecc72b4200d8af6
Berat Sherif <beroosheriff@gmail.com>
2022-11-23 20:29:39 +0200

    Add:Encapsulation - Exercise

---
+++ C# OOP/2.Encapsulation/Encapsulation - Exercise/01. Class Box Data/Box.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace _01._Class_Box_Data
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length,double width,double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            double result = 2 * (Length * Width) + 2 * (Length * Height) + 2 * (Width * Height);
+
+            return result;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            double result = (2 * Length * Height) + (2 * Width * Height);
+
+            return result;
+        }
+
+        public double Volume()
+        {
+            double result = Length * Width * Height;
+
+            return result;
+        }
+    }
+}
