7b7e8d7abca0078308afe99f6a44d89ae528f777
Berat Sherif <beroosheriff@gmail.com>
2022-11-30 17:46:27 +0200

    Add:new task

---
+++ C# OOP/2.Encapsulation/Encapsulation - Exercise/03.ShoppingSpree/Person.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ShoppingSpree
+{
+   public class Person
+    {
+        private string name;
+        private decimal money;
+        private List<Product> bagOfProducts;
+
+        public Person(string name, decimal money)
+        {
+            Name = name;
+            Money = money;
+            bagOfProducts = new List<Product>();
+        }
+
+        public IReadOnlyCollection<Product> BagOfProducts => this.bagOfProducts.AsReadOnly();
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public decimal Money
+        {
+            get => money;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+
+                money = value;
+            }
+        }
+
+        public bool AddProduct(Product product)
+        {
+            if (Money - product.Cost < 0)
+            {
+                return false;
+            }
+
+            bagOfProducts.Add(product);
+            Money -= product.Cost;
+
+            return true;
+        }
+    }
+}
---
+++ C# OOP/2.Encapsulation/Encapsulation - Exercise/03.ShoppingSpree/StartUp.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ShoppingSpree
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] persons = Console.ReadLine().Split(new char[] { '=', ';' }, StringSplitOptions.RemoveEmptyEntries);
+            string[] products = Console.ReadLine().Split(new char[] { '=', ';' }, StringSplitOptions.RemoveEmptyEntries);
+
+            Dictionary<string, Person> peopleList = new Dictionary<string, Person>();
+            Dictionary<string, Product> productList = new Dictionary<string, Product>();
+
+            try
+            {
+                //finding people and their money;
+                for (int i = 0; i < persons.Length; i+= 2)
+                {
+                    string name = persons[i];
+                    decimal money = decimal.Parse(persons[i + 1]);
+
+                    Person curPerson = new Person(name, money);
+                    peopleList.Add(name, curPerson);
+                }
+                
+
+                //finding products and their prices;
+                for (int i = 0; i < products.Length; i+= 2)
+                {
+                    string productName = products[i];
+                    decimal productPrice = decimal.Parse(products[i + 1]);
+
+                    Product curProduct = new Product(productName,productPrice);
+                    productList.Add(productName,curProduct);
+                }
+
+                while (true)
+                {
+                    string cmd = Console.ReadLine();
+                    if (cmd == "END")
+                    {
+                        break;
+                    }
+
+                    string[] command = cmd.Split();
+
+                    string personName = command[0];
+                    string productName = command[1];
+
+                    Person person = peopleList[personName];
+                    Product product = productList[productName];
+
+                    bool isAdd = person.AddProduct(product);
+
+                    if (!isAdd)
+                    {
+                        Console.WriteLine($"{personName} can't afford {productName}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{personName} bought {productName}");
+                    }
+                }
+
+                foreach (var peoples  in peopleList)
+                {
+                    string info = peoples.Value.BagOfProducts.Count > 0 ? string.Join(", ",peoples.Value.BagOfProducts.Select(x => x.Name)): "Nothing bought";
+
+                    Console.WriteLine($"{peoples.Key} - {info}");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
